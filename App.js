5f35171003e8fb0bb2a9ca735d3bc689479fe876
Corey Williams <coreywllms9@gmail.com>
2021-02-17 14:21:46 -0500

    expo set up

--- App.js
+++ App.js
@@ -1,21 +1,79 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, View, TouchableOpacity} from 'react-native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+import AddProductScreen from './src/screens/add-product-screen';
+import HomeScreen from './src/screens/home-screen';
+import {Button} from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import {withAuthenticator} from 'aws-amplify-react-native';
+import awsconfig from './aws-exports';
+import Amplify from 'aws-amplify';
+import 'react-native-gesture-handler';
+import {Auth} from 'aws-amplify';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+Amplify.configure({
+  ...awsconfig,
+  Analytics: {
+    disabled: true,
   },
 });
+
+const App = () => {
+  const Stack = createStackNavigator();
+  return (
+    <>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={({navigation}) => ({
+              title: 'Home',
+              headerStyle: {
+                backgroundColor: '#ff9300',
+              },
+              headerRight: () => (
+                <TouchableOpacity
+                  style={styles.addButton}
+                  onPress={() => navigation.navigate('AddProduct')}>
+                  <Icon name={'plus'} size={20} color="#000000" />
+                </TouchableOpacity>
+              ),
+              headerLeft: () => (
+                <View style={styles.logOutBtn}>
+                  <Button
+                    icon={<Icon name="sign-out" size={25} color="#000000" />}
+                    onPress={() =>Auth.signOut()}
+                    type="clear"
+                  />
+                </View>
+              ),
+            })}
+          />
+          <Stack.Screen
+            name="AddProduct"
+            buttonStyle={styles.addButton}
+            component={AddProductScreen}
+            options={{
+              title: 'Add Product',
+              headerStyle: {
+                backgroundColor: '#ff9300',
+              },
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
+};
+const styles = StyleSheet.create({
+  addButton: {
+    marginRight: 20,
+  },
+  logOutBtn: {
+    marginLeft: 10,
+  },
+});
+
+export default withAuthenticator(App);
